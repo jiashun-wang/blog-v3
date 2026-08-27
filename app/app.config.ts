e65b9d361b4e57e01d60cfbf6d576c4328959d8c
Jiashun Wang <wjs@ieee.org>
2026-08-27 22:43:03 +0800

    edit logo

--- app/app.config.ts
+++ app/app.config.ts
@@ -95,7 +95,7 @@ export default defineAppConfig({
 
 	/** 左侧栏顶部 Logo */
 	header: {
-		logo: 'https://weavatar.com/avatar/47c0f2e82b76d9b10eb3023df9e02e4e3fdbeaf5b74b842063f207971e7fbe7b?s=160',
+		logo: 'https://cdn.chum-bucket.site/image/20251006-175239-849.webp',
 		/** 展示标题文本，否则展示纯 Logo */
 		showTitle: true,
 		subtitle: blogConfig.subtitle,
